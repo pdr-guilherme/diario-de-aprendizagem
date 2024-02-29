35c6a8e487d0b362559beeccfdb59c68564e2eec
Pedro Guilherme <guilhermealves2848@gmail.com>
2024-02-29 20:48:43 -0300

    remover código inútil

--- diarios/admin.py
+++ diarios/admin.py
@@ -3,15 +3,7 @@ from django.utils.text import slugify
 
 from diarios.models import Entrada, Topico
 
-
 # Register your models here.
-class TopicoAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ["topico"]}
 
-
-# class EntradaAdmin(admin.ModelAdmin):
-#    prepopulated_fields = {"slug": [slugify("titulo")]}
-
-
-admin.site.register(Topico, TopicoAdmin)
+admin.site.register(Topico)
 admin.site.register(Entrada)
